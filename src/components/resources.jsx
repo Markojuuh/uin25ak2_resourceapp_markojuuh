3ff92e441002506ab37f90d4d30845e8615f19d7
codeawayjustcodeaway <112389416+Markojuuh@users.noreply.github.com>
2025-03-15 16:58:40 +0100

    oppdatering i komponent

--- src/components/resources.jsx
+++ src/components/resources.jsx
@@ -2,6 +2,6 @@ import React from "react";
 import resources from "../assets/javascript/ressurser";
 import pagetitle from "./pagetitle";
 
-const resources = ({ category }) => {
-    
+const Resources = ({ category }) => {
+
 }
