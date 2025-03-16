cc628fbefb70dc2be979eb13f8ed41312555dfdd
codeawayjustcodeaway <112389416+Markojuuh@users.noreply.github.com>
2025-03-16 14:00:54 +0100

    oppdatering av app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,12 @@ function App() {
     <Router>
       <Layout>
         <Routes>
-          <Route path="/" element={<Resources />} />
+          <Route path="/" element={<Resources category="html" />} />
+          <Route path="/html" element={<Resources category="html" />} />
+          <Route path="/css" element={<Resources category="css" />} />
+          <Route path="/javascript" element={<Resources category="javascript" />} />
+          <Route path="/react" element={<Resources category="react" />} />
+          <Route path="/sanity" element={<Resources category="sanity" />} />
         </Routes>
       </Layout> 
     </Router>
--- src/components/resources.jsx
+++ src/components/resources.jsx
@@ -3,5 +3,17 @@ import resources from "../assets/ressurser";
 import pagetitle from "./pagetitle";
 
 const Resources = ({ category }) => {
+    const filteredResources = resources.filter(resource => resource.category === category);
+    return (
+        <ul className="resources">
+            {filteredResources.map(resource => (
+                <li key={resource.id} className="resource">
+                    <h3>{resource.title}</h3>
+                    <p>{resource.description}</p>
+                    <a href={resource.url} target="_blank" rel="noopener noreferrer">Visit</a>
+                </li>
+            ))}
+        </ul>
+    )
 
 }
